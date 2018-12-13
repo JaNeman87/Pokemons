2c35811cf6a714aac28a7f0852706ac37106dc4c
Nele <nemanjaprvulovich@gmail.com>
2018-12-13 14:15:03 +0100

    update

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -4,7 +4,7 @@ import classes from "./Spinner.css";
 const spinner = () => {
   return (
     <div>
-      <div className={classes.loader}>Loading...</div>;
+      <div className={classes.loader}>Loading...</div>
       <h1 className={classes.spinnerText}>Please wait...</h1>
     </div>
   );
